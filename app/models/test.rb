420ed71fdbe8fee92227de53edb029712c71ad52
Dmarmy <viktoria.makhonya@gmail.com>
2019-10-24 20:00:11 +0300

    ActiveRecord Query Interface

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+  def self.test_list(category_name)
+    joins(' JOIN categories ON tests.category_id = categories.id ')
+        .where(categories: { title: category_name })
+        .order('tests.id DESC')
+        .pluck(:title)
+  end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_172959) do
+ActiveRecord::Schema.define(version: 2019_10_24_165620) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
     t.boolean "correct", default: false, null: false
-    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -29,19 +27,23 @@ ActiveRecord::Schema.define(version: 2019_10_21_172959) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text", null: false
-    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
     t.integer "level", default: 0
-    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_tests_on_category_id"
+    t.integer "category_id"
+  end
+
+  create_table "user_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,7 +53,4 @@ ActiveRecord::Schema.define(version: 2019_10_21_172959) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "answers", "questions"
-  add_foreign_key "questions", "tests"
-  add_foreign_key "tests", "categories"
 end
