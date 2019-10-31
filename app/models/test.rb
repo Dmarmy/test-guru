320f90ed1b83917acbd6ad1eba7d227182e1369b
Dmarmy <viktoria.makhonya@gmail.com>
2019-10-31 21:57:20 +0300

    fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,14 @@
 class Test < ApplicationRecord
+  belongs_to :category, optional: true
+  belongs_to :author, class_name: :User, foreign_key: :author_id, optional: true
+  has_many :questions, dependent: :destroy
+  has_many :tests_users, dependent: :destroy
+  has_many :users, through: tests_users,  dependent: :destroy
   def self.test_list(category_name)
     joins(:category)
         .where(categories: { title: category_name })
         .order(tests DESC)
         .pluck(:title)
   end
-  has_many :tests_users
-  has_many :users, through: tests_users
-  belongs_to :category, optional: true
-  belongs_to :author, class_name: :User, foreign_key: :author_id, optional: true
-  has_many :questions
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   def tests_passed(level)
     tests.where(tests: { level: level })
   end
-  has_many :tests_users
-  has_many :tests, through: tests_users
-  has_many :created_tests, class_name: :Test, foreign_key: :author_id
+  has_many :tests_users, dependent: :destroy
+  has_many :tests, through: tests_users, dependent: :destroy
+  has_many :created_tests, class_name: :Test, foreign_key: :author_id, dependent: :nullify
 end
