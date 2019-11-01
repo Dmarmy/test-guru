2b18f06124d74509e20b840ad6de4a8505bfb521
Dmarmy <viktoria.makhonya@gmail.com>
2019-11-01 15:02:48 +0300

    fixed

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,9 +3,9 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: :User, foreign_key: :author_id, optional: true
   has_many :questions, dependent: :destroy
   has_many :tests_users, dependent: :destroy
-  has_many :users, through: tests_users,  dependent: :destroy
-  def self.test_list(category_name)
-    joins(:category)
+  has_many :users, through: :tests_users
+    def self.test_list(category_name)
+      joins(:category)
         .where(categories: { title: category_name })
         .order(tests DESC)
         .pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
     tests.where(tests: { level: level })
   end
   has_many :tests_users, dependent: :destroy
-  has_many :tests, through: tests_users, dependent: :destroy
+  has_many :tests, through: :tests_users
   has_many :created_tests, class_name: :Test, foreign_key: :author_id, dependent: :nullify
 end
