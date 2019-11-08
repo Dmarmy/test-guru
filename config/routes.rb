03d686420e24895cf5310ae4d6da3a4949557400
Dmarmy <viktoria.makhonya@gmail.com>
2019-11-08 23:19:30 +0300

    extra routes removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tests do
-    resources :questions, shallow: true
+  resources :tests, except: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :questions, shallow: true, only: [:new, :create, :index, :show, :destroy]
   end
 end
