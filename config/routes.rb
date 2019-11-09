73b4041001977e786302e2948852b8b1b77614b8
Dmarmy <viktoria.makhonya@gmail.com>
2019-11-09 19:35:53 +0300

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tests, except: [:new, :create, :show, :edit, :update, :destroy] do
-    resources :questions, shallow: true, only: [:new, :create, :index, :show, :destroy]
+  resources :tests, only: [:index] do
+    resources :questions, shallow: true, except: [:edit, :update]
   end
 end
